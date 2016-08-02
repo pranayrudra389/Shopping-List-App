f9e280a4cf5dd7372a2012691fc549e84b120964
Pranay Kumar Rudra <Pranay Kumar Rudra>
2016-08-02 10:53:59 -0700

    Included bootstrap

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,4 +1,17 @@
 var app = angular.module("shoppingList", []);
 app.controller("myCtrl", function ($scope){
 	$scope.products = ["Milk", "Eggs", "Bread", "Yogurt"];
+	$scope.addItem = function () {
+		$scope.errormessage = "";
+		if (!$scope.addMe) {return;}
+		if ($scope.products.indexOf($scope.addMe) == -1){
+			$scope.products.push($scope.addMe);
+		}else {
+			$scope.errormessage = "The item is already in your shopping list.";
+		}
+	};
+	$scope.removeItem = function (x) {
+		$scope.errormessage = "";
+		$scope.products.splice(x, 1);
+	}
 });
